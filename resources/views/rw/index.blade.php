c80b23849a689a7116b62b42f3ba626cabdf7d95
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2024-03-18 01:20:46 +0700

    tipis tipis lek

--- resources/views/rw/index.blade.php
+++ resources/views/rw/index.blade.php
@@ -24,7 +24,7 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total Admin</h4>
+                                <h4>Total Penduduk</h4>
                             </div>
                             <div class="card-body">
                                 10
@@ -39,7 +39,7 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>News</h4>
+                                <h4>Kas</h4>
                             </div>
                             <div class="card-body">
                                 42
